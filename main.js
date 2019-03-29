7df9984a0cc4cfe2a3c09412c9d63429f3c95bc0
rosario2112 <34485145+rosario2112@users.noreply.github.com>
2019-03-28 20:44:48 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+var cards = ["queen","queen","king","king"];
+
+
+if(cardsInPlay[0] === cardsInPlay[1]){
+
+var flipCard = function(cardId){
+  alert("You found a match");
+  }else {
+  alert("Sorry try again");
+  }
+  console.log("User flipped" + cards[cardId]);
+};
+
+if (cardsInPlay == 2); 
